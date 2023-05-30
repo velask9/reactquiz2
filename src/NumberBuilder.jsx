97fcd871ea3648281ea4a6286dee724d7c1a321a
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-30 10:30:58 -0400

    NumberBuilder

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 
 export const NumberBuilder = () => {
-
+    let [counter, setCounter]=useState(0)
+    let[stringCounter, setStringCounter]= useState("")
+    
+    const handleAddNum =() => {
+        setCounter(++counter)
+        setStringCounter(stringCounter+counter)
+    }
+    
+    const handleReset= () => {
+        setCounter(0)
+        setStringCounter("")
+    }
+    
+    
     return (
         <>
-            <button>Add Number</button>
-            <button>Reset</button>
+            <button onClick={() => handleAddNum()}>Add Number</button>
+            <button onClick={() => handleReset()}>Reset</button>
             <br></br>
-            Numbers: 
+            Numbers: {stringCounter}
         </>
     )
 }
