f604627835996f9ea6dea9a035516cd98a3c061d
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-30 10:37:57 -0400

    Add Calculator

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const AddCalculator = () => {
+    let[total, setTotal]= useState(0)
+    const onHandleClick=(value)=>{
+        setTotal(total+=value)
+    }
+
 
     return (
         <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
+            <button onClick={()=>onHandleClick(1)}>Add 1</button>
+            <button onClick={()=>onHandleClick(2)}>Add 2</button>
+            <button onClick={()=>onHandleClick(3)}>Add 3</button>
             <br></br>
-            Total: 
+            Total:{total}
         </>
     )
 }
